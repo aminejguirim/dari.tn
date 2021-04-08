3d599c5d4bbd87bde8f2a9e383c543df1d268004
amine jguirim <amine.jeguirim.ess@gmail.com>
2021-04-08 21:45:11 +0100

    newversion11

--- src/test/java/com/spring/dev/home/AuthApp/cache/LoggedOutJwtTokenCacheTest.java
+++ src/test/java/com/spring/dev/home/AuthApp/cache/LoggedOutJwtTokenCacheTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.accolite.pru.health.AuthApp.cache;
+package com.spring.dev.home.AuthApp.cache;
 
 import org.junit.Before;
 import org.junit.Test;
--- src/test/java/com/spring/dev/home/AuthApp/security/JwtTokenValidatorTest.java
+++ src/test/java/com/spring/dev/home/AuthApp/security/JwtTokenValidatorTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.accolite.pru.health.AuthApp.security;
+package com.spring.dev.home.AuthApp.security;
 
 import org.junit.Before;
 import org.junit.Rule;
