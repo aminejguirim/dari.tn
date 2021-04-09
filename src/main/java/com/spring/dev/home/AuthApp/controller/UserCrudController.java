59a53b26941b7bbea00b6851dfa938cf3835b0a7
amine jguirim <amine.jeguirim.ess@gmail.com>
2021-04-09 01:11:18 +0100

    newversion13

--- src/main/java/com/spring/dev/home/AuthApp/controller/UserCrudController.java
+++ src/main/java/com/spring/dev/home/AuthApp/controller/UserCrudController.java
@@ -1,16 +1,21 @@
 package com.spring.dev.home.AuthApp.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.dev.home.AuthApp.model.User;
+import com.spring.dev.home.AuthApp.repository.UserCrudRepository;
 import com.spring.dev.home.AuthApp.service.IUserCrudService;
 
 @RestController
@@ -31,5 +36,9 @@ public class UserCrudController {
 	User user = userService.addUser(u);
 	return user;
 	}
-	
-}
+	@GetMapping("/retrieve-user/{user-id}")
+	@ResponseBody
+	public User retrieveUser(@PathVariable("user-id") String userId) {
+	return userService.retrieveUser(userId);
+	}
+	  }
